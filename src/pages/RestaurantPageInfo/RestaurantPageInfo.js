39c76ad18cce61583bab8e532b0594e0b286ce38
qwercik <erykandrzejewski@gmail.com>
2019-10-31 23:01:40 +0100

    Add more example description text in restaurant view page

--- src/pages/RestaurantPageInfo/RestaurantPageInfo.js
+++ src/pages/RestaurantPageInfo/RestaurantPageInfo.js
@@ -16,7 +16,11 @@ export default function RestaurantInfoPage() {
             name="Stołówka studencka"
             address="ul. Baraniaka 6"
             rating={4}
-            description="Lorem ipsum dolor sit amet"
+            description="Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore
+              dolore magna aliqua. Ut enim ad
+              minim veniam, quis nostrud"
           /> 
         </div>
         <div className="opening-hours">
@@ -26,7 +30,7 @@ export default function RestaurantInfoPage() {
           <h3>Komentarze</h3>
         </div>
       </div>
-      
+
       <BackButton />
     </div>
   );
