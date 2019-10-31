d448c3a98c24cbefe35438bc58d3d8400491226f
Ryszard Poznanski <ryszard.poznanski0@gmail.com>
2019-10-31 21:40:30 +0100

    Remove unused code

--- src/App.js
+++ src/App.js
@@ -4,23 +4,22 @@ import MainMap from './components/Map/MainMap';
 import NavBar from './components/NavBar/NavBar';
 import SearchBar from './components/SearchBar/SearchBar';
 import FilterModal from './components/FilterModal/FilterModal';
-import { usePosition } from './utils/customHooks';
 
-function App() {
+const App = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [position, error] = usePosition();
+
   const toggleModal = () => {
     setIsModalOpen(s => !s);
   };
-  if (error) alert(error);
+
   return (
     <div className="App">
-      <MainMap currentGeolocation={position} />
+      <MainMap />
       <SearchBar toggleModal={toggleModal} />
       <NavBar />
       <FilterModal modalData={{ isVisible: isModalOpen, toggleModal }} />
     </div>
   );
-}
+};
 
 export default App;
