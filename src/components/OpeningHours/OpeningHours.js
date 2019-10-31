a233603fdd4045fa460e9c3c5be54563410ed5b6
qwercik <erykandrzejewski@gmail.com>
2019-10-31 21:57:39 +0100

    Add OpeningHours component

---
+++ src/components/OpeningHours/OpeningHours.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function OpeningHours (props) {
+  return (
+    <div>
+      <h3>Godziny otwarcia</h3>
+      <ol>
+        <li>Pon - Pt</li>
+        <li>Sobota</li>
+        <li>Niedziela</li>
+      </ol>
+    </div>
+  );
+}
--- src/pages/RestaurantPageInfo.js
+++ src/pages/RestaurantPageInfo.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import PlaceDescription from '../components/PlaceDescription/PlaceDescription';
+import OpeningHours from '../components/OpeningHours/OpeningHours';
 
 export default function RestaurantInfoPage() {
   return (
     <div className="MapPage">
-      <h1>Restaurant info</h1>
+      <PlaceDescription
+        name="Stołówka studencka"
+        address="ul. Baraniaka 6"
+        rating={4}
+        description="Lorem ipsum dolor sit amet"
+      />
+      
+      <OpeningHours />
     </div>
   );
 }
