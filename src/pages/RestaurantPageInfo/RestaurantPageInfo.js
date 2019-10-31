c848b354dbc0c498fa43cc6a4c80d9d4ee19c440
qwercik <erykandrzejewski@gmail.com>
2019-10-31 22:33:12 +0100

    Add margin between resturant page view sections

--- src/pages/RestaurantPageInfo/RestaurantPageInfo.js
+++ src/pages/RestaurantPageInfo/RestaurantPageInfo.js
@@ -9,15 +9,22 @@ export default function RestaurantInfoPage() {
     <div className="RestaurantPageInfo">
       <img src={RestaurantPicture} alt="" className="restaurant-picture"/>
       
-      <div class="contentt">
-        <PlaceDescription
-          name="Stołówka studencka"
-          address="ul. Baraniaka 6"
-          rating={4}
-          description="Lorem ipsum dolor sit amet"
-        />
-
-        <OpeningHours />
+      <div className="restaurant-info-content">
+        <div>
+          <PlaceDescription
+            name="Stołówka studencka"
+            address="ul. Baraniaka 6"
+            rating={4}
+            description="Lorem ipsum dolor sit amet"
+          /> 
+        </div>
+        <div className="opening-hours">
+          <OpeningHours />
+        </div>
+        <div className="comments">
+          <h3>Komentarze</h3>
+        </div>
+        
       </div>
     </div>
   );
