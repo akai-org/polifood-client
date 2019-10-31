f6efa84b811be002b9ff0ebc2abc91eb5b512006
qwercik <erykandrzejewski@gmail.com>
2019-10-31 23:10:26 +0100

    Style opening hours component

--- src/components/OpeningHours/OpeningHours.js
+++ src/components/OpeningHours/OpeningHours.js
@@ -1,13 +1,37 @@
 import React from 'react';
+import "./OpeningHours.scss";
 
 export default function OpeningHours (props) {
   return (
     <div>
       <h3>Godziny otwarcia</h3>
-      <ol>
-        <li>Pon - Pt</li>
-        <li>Sobota</li>
-        <li>Niedziela</li>
+      <ol className="opening-hours-list">
+        <li className="opening-hours-element">
+          <div className="day-name">
+            Pon - Pt
+          </div>
+          <div className="hours-interval">
+            10:00 - 22:00
+          </div>
+        </li>
+
+        <li className="opening-hours-element">
+          <div className="day-name">
+            Sobota
+          </div>
+          <div className="hours-interval">
+            10:00 - 24:00
+          </div>
+        </li>
+
+        <li className="opening-hours-element">
+          <div className="day-name">
+            Niedziela
+          </div>
+          <div className="hours-interval">
+            12:00 - 24:00
+          </div>
+        </li>
       </ol>
     </div>
   );
