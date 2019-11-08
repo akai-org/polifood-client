e58fd4b578d66fe45a2e082b90f9c2b896383de8
Ryszard Poznanski <ryszard.poznanski0@gmail.com>
2019-11-08 15:15:06 +0100

    Implement MapContext logic

--- src/context/mapContext.js
+++ src/context/mapContext.js
@@ -5,8 +5,27 @@ const initState = {
   clientPosition: [],
   mapPosition: [52.403596, 16.950051],
   mapZoom: 16,
-  markers: [],
-  selectedMarker: {}
+  markers: [
+    {
+      id: '1',
+      position: [52.403596, 16.950051],
+      name: 'Centrum Wykładowe PP',
+      address: 'ul. Piotrowo 2'
+    },
+    {
+      id: '2',
+      position: [52.402573, 16.947926],
+      name: 'Centrum Sportu PP',
+      address: 'ul. Piotrowo 4'
+    }
+  ],
+  selectedMarker: {
+    id: '',
+    position: [],
+    name: '',
+    address: ''
+  },
+  popupActive: false
 };
 
 // reducer
@@ -23,6 +42,20 @@ const mapReducer = (state, action) => {
         ...state,
         mapPosition: action.position
       };
+    case 'open-popup':
+      return {
+        ...state,
+        popupActive: true,
+        mapZoom: 17,
+        mapPosition: action.marker.position,
+        selectedMarker: { ...action.marker }
+      };
+    case 'close-popup':
+      return {
+        ...state,
+        mapZoom: 16,
+        popupActive: false
+      };
     default:
       return state;
   }
@@ -35,14 +68,21 @@ const setMapZoom = dispatch => zoom => {
 };
 
 const setMapPosition = dispatch => position => {
-  console.log(position);
   dispatch({ type: 'set-position', position });
+};
+
+const setCurrentMarker = dispatch => marker => {
+  dispatch({ type: 'open-popup', marker });
+};
+
+const closePopup = dispatch => () => {
+  dispatch({ type: 'close-popup' });
 };
 
 // actions -->
 
 export const { Context, Provider } = createDataContext(
   mapReducer,
-  { setMapZoom, setMapPosition },
+  { setMapZoom, setMapPosition, setCurrentMarker, closePopup },
   initState
 );
