5a637a4b4ae37c7bc75a4f12f78fd9871f266514
Ryszard Poznanski <ryszard.poznanski0@gmail.com>
2019-11-08 15:14:41 +0100

    Add MapContext provider to global tree

--- src/context/DataContextWrapper.js
+++ src/context/DataContextWrapper.js
@@ -1,17 +1,12 @@
 import React from 'react';
 
 // example of provider import, please follow this pattern
-import { Provider as SampleContextProvider } from './SampleContext';
-import { Provider as AnotherContextProvider } from './AnotherContext';
+import { Provider as MapProvider } from './mapContext';
 
 const DataContextWrapper = ({ children }) => {
   // When you create new type of context, add new Provider to the tree down below
   // children element is our whole App
-  return (
-    <SampleContextProvider>
-      <AnotherContextProvider>{children}</AnotherContextProvider>
-    </SampleContextProvider>
-  );
+  return <MapProvider>{children}</MapProvider>;
 };
 
 // After approval of this idea, we need to wrap whole App with this DataContextWrapper
