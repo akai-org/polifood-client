c9bb8d493d52df97f6366f0b3e72a28afb7a7e83
qwercik <erykandrzejewski@gmail.com>
2019-11-11 20:45:52 +0100

    Toggle navbar entries

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,18 +5,29 @@ import navOption1 from '../../assets/icons/nav-option-1.svg';
 import navOption2 from '../../assets/icons/nav-option-2.svg';
 import navOption3 from '../../assets/icons/nav-option-3.svg';
 
+const links = [
+  { to: '/', src: navOption1, alt: 'icon-1' },
+  { to: '/restaurant-info', src: navOption2, alt: 'icon-2' },
+  { to: '/', src: navOption3, alt: 'icon-3' }
+];
+
+function toggleNavBarEntry() {
+
+}
+
 const NavBar = () => {
-  const links = [
-    { to: '/', src: navOption1, alt: 'icon-1' },
-    { to: '/restaurant-info', src: navOption2, alt: 'icon-2' },
-    { to: '/', src: navOption3, alt: 'icon-3' }
-  ];
+  const [activeEntryIndex, setActiveEntry] = useState(0);
 
   return (
     <div className="navbar">
-      {links.map(link => (
+      {links.map((link, index) => (
         <Link to={link.to}>
-          <img src={link.src} alt={link.alt} className="navbar__icon" />
+          <img
+            src={link.src}
+            alt={link.alt}
+            className={`navbar__icon ${activeEntryIndex == index ? 'navbar__active' : ''}`}
+            onClick={() => setActiveEntry(index)}
+          />
         </Link>
       ))}
     </div>
