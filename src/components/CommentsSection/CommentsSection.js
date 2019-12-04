e893f9d96080ab9556dd9f312d2fce99ce5f3a79
maslana146 <maslana146@gmail.com>
2019-12-04 20:06:22 +0100

    Burgers$comments done!

--- src/components/CommentsSection/CommentsSection.js
+++ src/components/CommentsSection/CommentsSection.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './CommentsSection.scss';
+import burger from '../../assets/icons/burger.svg';
+import burger2 from '../../assets/icons/burger-2.svg';
 
 const CommentsSection = (props) => {
     return(
@@ -7,16 +9,38 @@ const CommentsSection = (props) => {
         <h1 className="Header">Komentarze</h1>
         <div className="Comment">  
             <h3 className="Username">Dominik</h3>
+            <div className="burgers">
+                <img src={burger2} alt="icon-1" className="burger" />
+                <img src={burger2} alt="icon-1" className="burger" />
+                <img src={burger} alt="icon-1" className="burger" />
+                <img src={burger} alt="icon-1" className="burger" />
+                <img src={burger} alt="icon-1" className="burger" />
+            </div>
             <span className="date">20-09-2019</span>
             <p className="Commenttext" >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore dolore magna aliqua.</p>
         </div>
         <div className="Comment">  
             <h3 className="Username">Dominik</h3>
+            <div className="burgers">
+                <img src={burger2} alt="icon-1" className="burger" />
+                <img src={burger2} alt="icon-1" className="burger" />
+                <img src={burger2} alt="icon-1" className="burger" />
+                <img src={burger2} alt="icon-1" className="burger" />
+                <img src={burger} alt="icon-1" className="burger" />
+            </div>
             <span className="date">20-09-2019</span>
             <p className="Commenttext" >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore dolore magna aliqua.</p>
         </div>
         <div className="Comment">
-            <h3 className="had">Zostaw komentarz:</h3>
+            <h3>Zostaw komentarz:</h3>    
+                <input className="name" placeholder="Imię"></input>
+                <div className="burgers">
+                    <img src={burger} alt="icon-1" className="burger" />
+                    <img src={burger} alt="icon-1" className="burger" />
+                    <img src={burger} alt="icon-1" className="burger" />
+                    <img src={burger} alt="icon-1" className="burger" />
+                    <img src={burger} alt="icon-1" className="burger" />
+                </div>
             <textarea className="Area" placeholder="Remember, be nice!" rows="8"></textarea>
             <button type="submit" className="btn">Wyślij</button>
         </div>
